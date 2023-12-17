61c5e200bed7fb697d89ce615feeb0224d2b21ee
Sudip Neupane <sudipneupane@nchl.local>
2023-12-17 14:42:48 +0545

    custom auth entry point for response error message

--- src/main/java/com/example/financialtracker/security/SecurityConfiguration.java
+++ src/main/java/com/example/financialtracker/security/SecurityConfiguration.java
@@ -1,12 +1,8 @@
 package com.example.financialtracker.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,12 +19,13 @@ public class SecurityConfiguration {
 
     private final JwtFilter jwtFilter;
 
+    private final CustomAuthEntryPoint entryPoint;
     private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .exceptionHandling(ex -> ex.accessDeniedHandler(accessDeniedHandler))
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(entryPoint).accessDeniedHandler(accessDeniedHandler))
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(c -> c.configurationSource(corsFilter()))
                 .authorizeHttpRequests(requests -> requests
